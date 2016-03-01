35b537764e5f9c26ffb4fd28a6efe249393e90c2
Rakesh <rakeshbhatia87@gmail.com>
2016-03-01 09:48:31 -0800

    Checkpoint 20 address bloc menu: updated

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -4,20 +4,23 @@ RSpec.describe AddressBook do
   describe "attributes" do
     it "responds to entries" do
       book = AddressBook.new
+
       expect(book).to respond_to(:entries)
     end
   end
 
   it "initializes entries as an array" do
     book = AddressBook.new
+
     expect(book.entries).to be_an(Array)
   end
 
   it "initializes entries as empty" do
     book = AddressBook.new
+
     expect(book.entries.size).to eq(0)
   end
-  
+
   describe "#add_entry" do
     it "adds only one entry to the address book" do
       book = AddressBook.new
@@ -36,5 +39,4 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
   end
-
 end
